07ec603f9a9b5a14d14e9e888d3a2a5bd359c45a
Luke McCleary <luke.mccleary@gmail.com>
2021-12-22 13:12:55 -0500

    global grousp

--- boundary-config-module/groups.tf
+++ boundary-config-module/groups.tf
@@ -11,7 +11,7 @@ resource "boundary_group" "infra_admins" {
   description = "group for dev org admins"
   member_ids = compact([boundary_user.hermes.id,
   var.enable_oidc ? boundary_user.kelly[0].id : null])
-  scope_id = boundary_scope.core_infra.id
+  scope_id = boundary_scope.global.id
 }
 
 resource "boundary_group" "hpc_admins" {
@@ -19,12 +19,12 @@ resource "boundary_group" "hpc_admins" {
   description = "group for global admins"
   member_ids = compact([boundary_user.zues.id,
   var.enable_oidc ? boundary_user.dave[0].id : null])
-  scope_id = boundary_scope.hcp.id
+  scope_id = boundary_scope.global.id
 }
 
 resource "boundary_group" "read_only" {
   name        = "read_only"
   description = "ready and list access only"
   member_ids = [boundary_user.hermes.id]
-  scope_id = boundary_scope.hcp.id
+  scope_id = boundary_scope.global.id
 }
