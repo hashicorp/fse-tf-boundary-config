381c48bb548783d11661133523034fd198351e6b
Luke McCleary <luke.mccleary@gmail.com>
2022-01-04 12:33:11 -0500

    oidc vars enabled

--- main.tf
+++ main.tf
@@ -31,11 +31,11 @@ data "tfe_outputs" "vault" {
 
 module "base-config" {
 source = "./boundary-config-module"
-#oidc_issuer        = var.oidc_issuer
-#oidc_client_secret = var.oidc_client_secret
-#oidc_client_id     = var.oidc_client_id
-#oidc_subject2      = var.oidc_subject2
-#oidc_subject1      = var.oidc_subject1
+oidc_issuer        = var.oidc_issuer
+oidc_client_secret = var.oidc_client_secret
+oidc_client_id     = var.oidc_client_id
+oidc_subject2      = var.oidc_subject2
+oidc_subject1      = var.oidc_subject1
 enable_oidc = var.enable_oidc
 vault_private_ip = data.tfe_outputs.vault.values.vault_private_ip
 controller_private_ip = data.tfe_outputs.infra.values.controller_private_ip
