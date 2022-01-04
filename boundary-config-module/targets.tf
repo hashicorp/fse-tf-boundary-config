ed491443f92a25d6f0459658c1a6100a9e87d8df
Luke McCleary <luke.mccleary@gmail.com>
2022-01-04 11:58:41 -0500

    removed psql target

--- boundary-config-module/targets.tf
+++ boundary-config-module/targets.tf
@@ -22,16 +22,3 @@ resource "boundary_target" "vault_ui" {
     boundary_host_set.vault_servers.id
   ]
 }
-
-resource "boundary_target" "postgres_db" {
-  type                     = "tcp"
-  name                     = "postgres_db"
-  description              = "postgres database"
-  #worker_filter            = "\"/region\" == \"/${var.region}\""
-  scope_id                 = boundary_scope.db_infra_proj.id
-  session_connection_limit = -1
-  default_port             = 5432
-  host_set_ids = [
-    boundary_host_set.database_servers.id
-  ]
-}
