a5d1440778f0d492d305184462f09bfb7bb44064
Luke McCleary <luke.mccleary@gmail.com>
2022-01-04 11:16:44 -0500

    ttl updates

--- vault-cs-module/roles.tf
+++ vault-cs-module/roles.tf
@@ -2,6 +2,8 @@ resource "vault_database_secret_backend_role" "analyst_role" {
   backend = vault_mount.psql.path
   name    = "analyst"
   db_name = vault_database_secret_backend_connection.northwind_connection.name
+  default_ttl = "3m"
+  max_ttl = "30m"
   creation_statements = [
     "create role \"{{name}}\" with login password '{{password}}' valid until '{{expiration}}' inherit;",
   "grant northwind_analyst to \"{{name}}\";"]
@@ -11,6 +13,8 @@ resource "vault_database_secret_backend_role" "dba_role" {
   backend = vault_mount.psql.path
   name    = "dba"
   db_name = vault_database_secret_backend_connection.northwind_connection.name
+  default_ttl = "3m"
+  max_ttl = "30m"
   creation_statements = ["create role \"{{name}}\" with login password '{{password}}' valid until '{{expiration}}' inherit;",
   "grant northwind_dba to \"{{name}}\";"]
 }
