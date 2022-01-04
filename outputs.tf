7f3f74ec21910b04c800e532b889f4169e374664
Luke McCleary <luke.mccleary@gmail.com>
2022-01-04 11:36:14 -0500

    sense

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,10 @@
 
 output "vault_token" {
   value = var.vault_token
-  sensitive = false
+  sensitive = true
 }
 
 output "boundary_vault_token" {
   value = module.vault-cs.boundary_vault_token
+  sensitive = true
 }
