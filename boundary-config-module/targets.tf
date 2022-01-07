8afaebd70e4173533d4c09bf3028ba508e54c17c
Luke McCleary <luke.mccleary@gmail.com>
2022-01-07 12:59:57 -0500

    removed ui target

--- boundary-config-module/targets.tf
+++ boundary-config-module/targets.tf
@@ -10,15 +10,15 @@ resource "boundary_target" "vault_ssh" {
   ]
 }
 
-resource "boundary_target" "vault_ui" {
-  type                     = "tcp"
-  name                     = "vault appplication front end"
-  description              = "hashicorp vault cluster"
-  #worker_filter            = "\"/region\" == \"/${var.region}\""
-  scope_id                 = boundary_scope.db_infra_proj.id
-  session_connection_limit = 0
-  default_port             = 8002
-  host_set_ids = [
-    boundary_host_set.vault_servers.id
-  ]
-}
+#resource "boundary_target" "vault_ui" {
+#  type                     = "tcp"
+#  name                     = "vault appplication front end"
+#  description              = "hashicorp vault cluster"
+#  #worker_filter            = "\"/region\" == \"/${var.region}\""
+#  scope_id                 = boundary_scope.db_infra_proj.id
+#  session_connection_limit = 0
+#  default_port             = 8002
+#  host_set_ids = [
+#    boundary_host_set.vault_servers.id
+#  ]
+#}
